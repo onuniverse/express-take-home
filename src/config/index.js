ecfde11531719dcbd6acc193fa4e06251710291f
Marcus Smith <marcus.smith@hotmail.co.jp>
2022-03-21 21:23:27 -0400

    Add env setup and discogs API config variable

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   databaseUrl: process.env.DATABASE_URL,
+  discogsApiToken: process.env.DISCOGS_API_TOKEN,
   logLevel: process.env.LOG_LEVEL,
   port: Number(process.env.PORT),
 }
